94bb2dadb1168053574886180d12a2619c39bd74
GeorgH93 <git@georgh93.eu>
2019-12-17 22:51:32 +0100

    Improve input processing

--- src/at/pcgamingfreaks/WorldGuardBlockRestricter/Bukkit/Flags/BlockMaterialFlag.java
+++ src/at/pcgamingfreaks/WorldGuardBlockRestricter/Bukkit/Flags/BlockMaterialFlag.java
@@ -1,13 +1,20 @@
 package at.pcgamingfreaks.WorldGuardBlockRestricter.Bukkit.Flags;
 
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.world.item.ItemType;
+import com.sk89q.worldedit.world.registry.LegacyMapper;
 import com.sk89q.worldguard.protection.flags.Flag;
 import com.sk89q.worldguard.protection.flags.FlagContext;
 import com.sk89q.worldguard.protection.flags.InvalidFlagFormat;
 
 import org.bukkit.Material;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class BlockMaterialFlag extends Flag<Material>
 {
+	private static final Pattern OLD_ITEM_PATTERN = Pattern.compile("^(?<id>\\d+)(:(?<data>\\d+))?$");
 	public BlockMaterialFlag(String name)
 	{
 		super(name);
@@ -16,35 +23,37 @@ public class BlockMaterialFlag extends Flag<Material>
 	@Override
 	public Material parseInput(FlagContext flagContext) throws InvalidFlagFormat
 	{
-		//TODO
 		String input = flagContext.getUserInput();
-		Material mat = Material.getMaterial(input);
-		//TODO make it more user friendly to input a material
+		Material mat = Material.matchMaterial(input);
+		if(mat == null)
+		{
+			Matcher matcher = OLD_ITEM_PATTERN.matcher(input);
+			if(matcher.matches())
+			{
+				int id = Integer.parseUnsignedInt(matcher.group("id")), data = 0;
+				String dataString = matcher.group("data");
+				if(dataString != null && !dataString.isEmpty()) data = Integer.parseUnsignedInt(dataString);
+				ItemType itemType = LegacyMapper.getInstance().getItemFromLegacy(id, data);
+				if(itemType != null) mat = BukkitAdapter.adapt(itemType);
+			}
+		}
+		if(mat == null) throw new InvalidFlagFormat("The material \"" + input + "\" is unknown.");
 		return mat;
 	}
 
 	@Override
 	public Material unmarshal(Object o)
 	{
-		return this.loadFromDb((String) o);
-	}
-
-	@Override
-	public Object marshal(Material t)
-	{
-		return this.saveToDb(t);
-	}
-
-	public Material loadFromDb(String str)
-	{
+		String str = (String) o;
 		Material mat = Material.getMaterial(str);
 		if(mat == null) mat = Material.getMaterial(str, true);
 		return mat;
 	}
 
-	public String saveToDb(Material o)
+	@Override
+	public Object marshal(Material t)
 	{
-		return o.name();
+		return t.name();
 	}
 }
 
